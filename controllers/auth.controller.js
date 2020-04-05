0027b84e1bc5ca85902d716b6c94233bf3aed33e
Cao Hoang Anh <caohoanganh2000@gmail.com>
2020-04-05 10:24:59 +0700

    auth/login

---
+++ controllers/auth.controller.js
@@ -0,0 +1,9 @@
+const db = require('../db')
+const shortid = require('shortid')
+
+module.exports.login = function(req, res){
+	res.render('./auth/login',{ 
+		
+	})
+}
+
--- index.js
+++ index.js
@@ -2,19 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 const userRoute = require('./routes/user.route')
+const authRoute = require('./routes/auth.route')
+var cookieParser = require('cookie-parser')
 
 app.set('view engine', 'pug')
 app.set('views', './views')
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
+app.use(cookieParser())
 
 
 
 app.get('/', (req, res) => res.render('index'))
 
 app.use('/users', userRoute)
-
+app.use('/auth', authRoute)
 
 
 
