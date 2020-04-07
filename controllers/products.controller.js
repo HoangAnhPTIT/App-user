744e515282e06c89d0e6ff640a43b0886d74b4cb
Cao Hoang Anh <caohoanganh2000@gmail.com>
2020-04-07 22:17:15 +0700

    Pagination products

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,9 +1,15 @@
 
 const db = require('../db')
 
-module.exports.index = (req, res) => {
+module.exports.index = function(req, res)  {
+	var page = parseInt(req.query.page)
+	var perPage = 6
+	var start = (page - 1) * perPage
+	var end = page * perPage
 	res.render('product/product',{
-		products: db.get('products').value()
+		
+		products: db.get('products').value().slice(start, end)
+
 	})
 
 }
